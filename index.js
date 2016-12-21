b863007ffb16f940a9c47e743d0ad4f6e341c750
Ryan Newton <newton1988@gmail.com>
2016-12-21 17:35:35 -0500

    Connects server to the router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const router = require('./router');
 
 const app = express();
 
@@ -7,6 +8,9 @@ const mongoose = require('mongoose');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth');
+
+// Set up routing
+router(app);
 
 // Server Setup
 // Sets the server port based on the environment variable, or to a default of 3090
