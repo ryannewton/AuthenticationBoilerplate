de9a68bade12325d65f61702df1244fcaeb8a371
Ryan Newton <newton1988@gmail.com>
2016-12-21 17:26:53 -0500

    Adds check in signup for if email is already registered

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -10,13 +10,22 @@ exports.signup = function(req, res, next) {
 		return res.status(422).send({ error: 'You must provide an email and password' });
 	}
 
-	const user = new User({
-		email: email,
-		password: password
-	});
+	// Checks if the email address is already registered
+	User.findOne({ email: email }, function(err, existingUser) {
+		// If the email was found in the database
+		if(existingUser) {
+			return res.status(422).send({ error: 'Email is in use' });
+		}
 
-	// Save profile to database
-	user.save(function(err) {
-		res.json({ message: "Profile created" });
+		// If email is not in the database, we can create a new account
+		const user = new User({
+			email: email,
+			password: password
+		});
+
+		// Save profile to database
+		user.save(function(err) {
+			res.json({ message: "Profile created" });
+		});
 	});
 }
