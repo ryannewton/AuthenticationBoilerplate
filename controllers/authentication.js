ce840555583ac790eea0f5a26c84d04ffa88d1d1
Ryan Newton <newton1988@gmail.com>
2016-12-21 17:23:40 -0500

    Adds check that signup credentials are not blank

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,6 +4,12 @@ exports.signup = function(req, res, next) {
 	const email = req.body.email;
 	const password = req.body.password;
 
+	// If no email or password was given, return an error
+	if(!email || !password) {
+		// Note: 422 error means unprocessible entity
+		return res.status(422).send({ error: 'You must provide an email and password' });
+	}
+
 	const user = new User({
 		email: email,
 		password: password
