8d14c5b5044bdea9122e88af5003caba1cd71c02
Ryan Newton <newton1988@gmail.com>
2016-12-22 12:24:47 -0800

    Looks up email in local passport strategy. Need to add password comparison

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,18 @@
 const passport = require('passport');
 const User = require('../models/user');
-LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local');
 
 // Create local strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+	User.findOne({ email: email }, function(err, user) {
+		// if there's an error searching the database
+		if(err) { return done(err); }
+
+		// if the user isn't found
+		if(!user) { return done(null, false); }
+
+		// check password
+		// TO DO: create function in user model to compare password
+	})
+});
