6d49f0227c506e03158c8441e2adb3984b15caa9
Ryan Newton <newton1988@gmail.com>
2016-12-22 17:20:57 -0800

    Adds signin to router, with authentication

--- router.js
+++ router.js
@@ -1,5 +1,11 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// Note: passport uses cookies by default. {session:false} stops this since we are using a token instead
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
 	app.post('/signup', Authentication.signup);
+	app.post('/signin', requireSignin, Authentication.signin);
 }
