9c906abcadf9a5bd4012f7bf529c55fadf1e3cc5
Ryan Newton <newton1988@gmail.com>
2017-01-18 16:46:10 -0500

    Adds example route that requires a user to be authenticated

--- router.js
+++ router.js
@@ -3,9 +3,15 @@ const passportService = require('./services/passport');
 const passport = require('passport');
 
 // Note: passport uses cookies by default. {session:false} stops this since we are using a token instead
+const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
+	// Example route that requires authentication before it can be accessed
+	app.get('/hidden', requireAuth, function(req, res) {
+		// This is the hidden content
+		res.send({ message: "You're in!" });
+	});
 	app.post('/signup', Authentication.signup);
 	app.post('/signin', requireSignin, Authentication.signin);
 }
