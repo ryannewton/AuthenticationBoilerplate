b0b199770636fe9c68790e067de89a118379ca3e
Ryan Newton <newton1988@gmail.com>
2017-01-18 15:59:44 -0500

    Adds jwtLogin to passport

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const User = require('../models/user');
 const LocalStrategy = require('passport-local');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const config = require('../config');
 
 // Create local strategy
 const localOptions = { usernameField: 'email' };
@@ -23,5 +26,26 @@ const localLogin = new LocalStrategy(localOptions, function(email, password, don
 	});
 });
 
+// Setup options for JWT Strategy
+const jwtOptions = {
+	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+	secretOrKey: config.secret
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+	User.findById(payload.sub, function(err, user) {
+		if(err) { return done(err, false); }
+
+		// If user was successfully found
+		if(user) {
+			done(null, user);
+		} else {
+			done(null, false);
+		}
+	});
+});
+
 // Connects strategies to passport
+passport.use(jwtLogin);
 passport.use(localLogin);
