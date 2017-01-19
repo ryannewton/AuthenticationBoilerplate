30fba31f7fe49e1609f1d466b16e6f36a1f9de83
Ryan Newton <newton1988@gmail.com>
2017-01-19 18:25:12 -0500

    Adds initial structure to Header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+	renderLinks() {
+		// If signed in - display 'Sign out' Link
+		// If not signed in - display 'Sign up' and 'Sign in' Links
+	}
+
 	render() {
 		return (
-			<div>Header</div>
+			<nav className="navbar navbar-light">
+				<Link to="/" className="navbar-brand">Redux Auth</Link>
+				<ul className="nav navbar-nav">
+					{this.renderLinks()}
+				</ul>
+			</nav>
 		);
 	}
 }
