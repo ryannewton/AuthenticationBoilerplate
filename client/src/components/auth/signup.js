3d7655599060b2ddff40b1a00d3b9bf5f2628763
Ryan Newton <newton1988@gmail.com>
2017-01-20 17:56:32 -0500

    React boilerplate for Signup component

---
+++ client/src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReduxForm from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+	render() {
+		return (
+			<div>Testing123</div>
+		);
+	}
+}
+
+export default Signup;
