e07b74186788435532f2e235094d616243a7f00e
Ryan Newton <newton1988@gmail.com>
2017-01-21 14:58:53 -0500

    React boilerplate for Feature component

---
+++ client/src/components/feature.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+	render() {
+		return (
+			<div>Secret section! Congrats, you're authenticated!</div>
+		);
+	}
+}
+
+export default Feature;
