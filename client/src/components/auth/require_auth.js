ad0ec1ea14abeb9dad9eae8aa3e9bb56961ee127
Ryan Newton <newton1988@gmail.com>
2017-01-21 15:49:14 -0500

    Authenticated HOC reroutes to /signin if user is not authenticated

--- client/src/components/auth/require_auth.js
+++ client/src/components/auth/require_auth.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
-  class Authentication extends Component {
-    render() {
-      return <ComposedComponent {...this.props} />
-    }
-  }
+	class Authentication extends Component {
+		// Gives access to this.context.router
+		static contextTypes = {
+			router: React.PropTypes.object
+		}
 
-  return Authentication;
+		componentWillMount() {
+			// If user is not authenticated, navigate to /signin
+			if(!this.props.authenticated) {
+				this.context.router.push('/signin');
+			}
+		}
+
+		render() {
+			return <ComposedComponent {...this.props} />
+		}
+	}
+
+	function mapStateToProps(state) {
+		return { authenticated: state.auth.authenticated };
+	}
+
+	return connect(mapStateToProps)(Authentication);
 }
