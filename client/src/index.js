de06747551e5a1753895a0ce3299154f1e359a53
Ryan Newton <newton1988@gmail.com>
2017-01-21 16:51:51 -0500

    Before page loads, updates state.auth.authenticated if user has a token

--- client/src/index.js
+++ client/src/index.js
@@ -12,11 +12,22 @@ import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import Landing from './components/landing';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+if (token) {
+	// If they have a token, set client state to authenticated before page loads
+	// Note: If a malicious user sets a fake token, the client will allow
+	//  them to navigate to protected routes, but the server will not send protected data
+	store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Provider store={store}>
 		<Router history={browserHistory}>
 			<Route path="/" component={App}>
 				<IndexRoute component = {Landing} />
